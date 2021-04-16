b55620b1faded5bcfafb8fa2b37adff851bfbe81
tera1707 <Masa1017>
2021-04-16 23:52:18 +0900

    微修正

--- serviceJikken1/x64/Debug/ServiceTestBiuld.bat
+++ serviceJikken1/x64/Debug/ServiceTestBiuld.bat
@@ -4,25 +4,17 @@ set SOLUTIONPATH="C:\Users\tera1\Desktop\test\serviceJikken1\serviceJikken1.sln"
 
 set SERVICENAME="SvcName"
 set SERVICEDISPNAME=%SERVICENAME%
-set BINPATH=C:\Users\tera1\Desktop\test\serviceJikken1\Debug\serviceJikken1.exe
+set BINPATH=C:\Users\tera1\Desktop\test\serviceJikken1\x64\Debug\serviceJikken1.exe
 set DISCRIPTION="Svc Description..."
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
 echo -サービスのプロセスを無理やり終了させる
-taskkill /f /im %BINPATH%
+rem taskkill /f /im %BINPATH%
+taskkill /f /im serviceJikken1.exe
 
 echo -サービスをアンインストール
 sc delete %SERVICENAME%
-
-echo -ソリューションをリビルドする
-MSBuild %SOLUTIONPATH% /t:clean;rebuild /p:Configuration=Debug;Platform="x86" > nul
-if %ERRORLEVEL% neq 0 (
-    echo ErrorLevel:%ERRORLEVEL%
-    echo ビルド失敗
-    pause
-    exit
-)
 
 echo -サービスをインストール
 rem 試した限り、binPathにはフルパスを指定しないとうまくいかない
@@ -34,6 +26,6 @@ sc description %SERVICENAME% %DISCRIPTION%
 echo -サービスをスタート
 rem sc start %SERVICENAME%
 net start %SERVICENAME%
-pause
+
 
 
