3d8eaf3fab3374a055a4a74c1a4fca21d3630b13
tera1707 <Masa1017>
2022-03-01 01:27:58 +0900

    aaa

---
+++ CppPipeServerClientJikken/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,408 @@
+﻿// WindowsProject1.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+#include <windows.h> 
+#include <windowsx.h> 
+#include <stdio.h> 
+#include <conio.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+#include "framework.h"
+#include "WindowsProject1.h"
+#include "resource.h"
+#include <string>
+#include <time.h>
+
+#define BUFSIZE 512
+
+DWORD WINAPI InstanceThread(LPVOID);
+VOID GetAnswerToRequest(LPTSTR, LPTSTR, LPDWORD);
+
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+HWND ghDlg = 0;
+// このコード モジュールに含まれる関数の宣言を転送します:
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK MyDlgProc(HWND, UINT, WPARAM, LPARAM);
+
+void Log(std::wstring txt)
+{
+    auto t = time(nullptr);
+    auto tmv = tm();
+    auto error = localtime_s(&tmv, &t); // ローカル時間(タイムゾーンに合わせた時間)を取得
+    WCHAR buf[256] = { 0 };
+    wcsftime(buf, 256, L"%Y/%m/%d %H:%M:%S ", &tmv);
+
+    std::wstring log = buf + txt;
+
+    auto hList = GetDlgItem(ghDlg, IDC_LIST1);
+    ListBox_InsertString(hList, 0, log.c_str());
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    hInst = hInstance;
+
+    DialogBox(hInst, L"MyTestDlgBase_Main", NULL, (DLGPROC)MyDlgProc);
+
+    return (int)0;
+}
+
+int PipeClient()
+{
+    HANDLE hPipe;
+    LPCTSTR lpvMessage = TEXT("Default message from client.");
+    TCHAR  chBuf[BUFSIZE];
+    BOOL   fSuccess = FALSE;
+    DWORD  cbRead, cbToWrite, cbWritten, dwMode;
+    LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\mynamedpipe");
+
+    // Try to open a named pipe; wait for it, if necessary. 
+
+    Log(L"パイプクライアント開始");
+
+    while (1)
+    {
+        hPipe = CreateFile(
+            lpszPipename,   // pipe name 
+            GENERIC_READ |  // read and write access 
+            GENERIC_WRITE,
+            0,              // no sharing 
+            NULL,           // default security attributes
+            OPEN_EXISTING,  // opens existing pipe 
+            0,              // default attributes 
+            NULL);          // no template file 
+
+      // Break if the pipe handle is valid. 
+
+        if (hPipe != INVALID_HANDLE_VALUE)
+        {
+            Log(L"hPipe != INVALID_HANDLE_VALUE");
+            break;
+        }
+
+        // Exit if an error other than ERROR_PIPE_BUSY occurs. 
+        auto err = GetLastError();
+        if (err != ERROR_PIPE_BUSY)
+        {
+            Log(L"Could not open pipe. GLE=" + std::to_wstring(err));
+            _tprintf(TEXT("Could not open pipe. GLE=%d\n"), err);
+            return -1;
+        }
+        else
+        {
+            Log(L"pipe ビジーです：" + std::to_wstring(err));
+        }
+
+        // All pipe instances are busy, so wait for 20 seconds. 
+
+        if (!WaitNamedPipe(lpszPipename, 20000))
+        {
+            Log(L"WaitNamedPipe タイムアウト");
+            printf("Could not open pipe: 20 second wait timed out.");
+            return -1;
+        }
+    }
+
+    Log(L"パイプopen成功");
+    // The pipe connected; change to message-read mode. 
+
+    dwMode = PIPE_READMODE_MESSAGE;
+    fSuccess = SetNamedPipeHandleState(
+        hPipe,    // pipe handle 
+        &dwMode,  // new pipe mode 
+        NULL,     // don't set maximum bytes 
+        NULL);    // don't set maximum time 
+    if (!fSuccess)
+    {
+        auto err = GetLastError();
+        Log(L"SetNamedPipeHandleState failed. GLE=" + std::to_wstring(err));
+        //_tprintf(TEXT("SetNamedPipeHandleState failed. GLE=%d\n"), GetLastError());
+        return -1;
+    }
+    else
+    {
+        Log(L"SetNamedPipeHandleState 成功");
+    }
+    // Send a message to the pipe server. 
+
+    cbToWrite = (lstrlen(lpvMessage) + 1) * sizeof(TCHAR);
+    _tprintf(TEXT("Sending %d byte message: \"%s\"\n"), cbToWrite, lpvMessage);
+
+    fSuccess = WriteFile(
+        hPipe,                  // pipe handle 
+        lpvMessage,             // message 
+        cbToWrite,              // message length 
+        &cbWritten,             // bytes written 
+        NULL);                  // not overlapped 
+
+    if (!fSuccess)
+    {
+        _tprintf(TEXT("WriteFile to pipe failed. GLE=%d\n"), GetLastError());
+        return -1;
+    }
+
+    printf("\nMessage sent to server, receiving reply as follows:\n");
+
+    do
+    {
+        // Read from the pipe. 
+
+        fSuccess = ReadFile(
+            hPipe,    // pipe handle 
+            chBuf,    // buffer to receive reply 
+            BUFSIZE * sizeof(TCHAR),  // size of buffer 
+            &cbRead,  // number of bytes read 
+            NULL);    // not overlapped 
+
+        if (!fSuccess && GetLastError() != ERROR_MORE_DATA)
+            break;
+
+        _tprintf(TEXT("\"%s\"\n"), chBuf);
+    } while (!fSuccess);  // repeat loop if ERROR_MORE_DATA 
+
+    if (!fSuccess)
+    {
+        _tprintf(TEXT("ReadFile from pipe failed. GLE=%d\n"), GetLastError());
+        return -1;
+    }
+
+    printf("\n<End of message, press ENTER to terminate connection and exit>");
+    _getch();
+
+    CloseHandle(hPipe);
+
+    return 0;
+}
+int PipeServer()
+{
+    BOOL   fConnected = FALSE;
+    DWORD  dwThreadId = 0;
+    HANDLE hPipe = INVALID_HANDLE_VALUE, hThread = NULL;
+    LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\mynamedpipe");
+
+    for (;;)
+    {
+        _tprintf(TEXT("\nPipe Server: Main thread awaiting client connection on %s\n"), lpszPipename);
+        hPipe = CreateNamedPipe(
+            lpszPipename,             // pipe name 
+            PIPE_ACCESS_DUPLEX,       // read/write access 
+            PIPE_TYPE_MESSAGE |       // message type pipe 
+            PIPE_READMODE_MESSAGE |   // message-read mode 
+            PIPE_WAIT,                // blocking mode 
+            PIPE_UNLIMITED_INSTANCES, // max. instances  
+            BUFSIZE,                  // output buffer size 
+            BUFSIZE,                  // input buffer size 
+            0,                        // client time-out 
+            NULL);                    // default security attribute 
+
+        if (hPipe == INVALID_HANDLE_VALUE)
+        {
+            _tprintf(TEXT("CreateNamedPipe failed, GLE=%d.\n"), GetLastError());
+            return -1;
+        }
+
+        // Wait for the client to connect; if it succeeds, 
+        // the function returns a nonzero value. If the function
+        // returns zero, GetLastError returns ERROR_PIPE_CONNECTED. 
+
+        fConnected = ConnectNamedPipe(hPipe, NULL) ?
+            TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+
+        if (fConnected)
+        {
+            printf("Client connected, creating a processing thread.\n");
+
+            // Create a thread for this client. 
+            hThread = CreateThread(
+                NULL,              // no security attribute 
+                0,                 // default stack size 
+                InstanceThread,    // thread proc
+                (LPVOID)hPipe,    // thread parameter 
+                0,                 // not suspended 
+                &dwThreadId);      // returns thread ID 
+
+            if (hThread == NULL)
+            {
+                _tprintf(TEXT("CreateThread failed, GLE=%d.\n"), GetLastError());
+                return -1;
+            }
+            else CloseHandle(hThread);
+        }
+        else
+            // The client could not connect, so close the pipe. 
+            CloseHandle(hPipe);
+    }
+    return 0;
+}
+DWORD WINAPI InstanceThread(LPVOID lpvParam)
+// This routine is a thread processing function to read from and reply to a client
+// via the open pipe connection passed from the main loop. Note this allows
+// the main loop to continue executing, potentially creating more threads of
+// of this procedure to run concurrently, depending on the number of incoming
+// client connections.
+{
+    HANDLE hHeap = GetProcessHeap();
+    TCHAR* pchRequest = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+    TCHAR* pchReply = (TCHAR*)HeapAlloc(hHeap, 0, BUFSIZE * sizeof(TCHAR));
+
+    DWORD cbBytesRead = 0, cbReplyBytes = 0, cbWritten = 0;
+    BOOL fSuccess = FALSE;
+    HANDLE hPipe = NULL;
+
+    // Do some extra error checking since the app will keep running even if this
+    // thread fails.
+
+    if (lpvParam == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL value in lpvParam.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    if (pchRequest == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchReply != NULL) HeapFree(hHeap, 0, pchReply);
+        return (DWORD)-1;
+    }
+
+    if (pchReply == NULL)
+    {
+        printf("\nERROR - Pipe Server Failure:\n");
+        printf("   InstanceThread got an unexpected NULL heap allocation.\n");
+        printf("   InstanceThread exitting.\n");
+        if (pchRequest != NULL) HeapFree(hHeap, 0, pchRequest);
+        return (DWORD)-1;
+    }
+
+    // Print verbose messages. In production code, this should be for debugging only.
+    printf("InstanceThread created, receiving and processing messages.\n");
+
+    // The thread's parameter is a handle to a pipe object instance. 
+
+    hPipe = (HANDLE)lpvParam;
+
+    // Loop until done reading
+    while (1)
+    {
+        // Read client requests from the pipe. This simplistic code only allows messages
+        // up to BUFSIZE characters in length.
+        fSuccess = ReadFile(
+            hPipe,        // handle to pipe 
+            pchRequest,    // buffer to receive data 
+            BUFSIZE * sizeof(TCHAR), // size of buffer 
+            &cbBytesRead, // number of bytes read 
+            NULL);        // not overlapped I/O 
+
+
+        if (!fSuccess || cbBytesRead == 0)
+        {
+            if (GetLastError() == ERROR_BROKEN_PIPE)
+            {
+                Log(L"InstanceThread: client disconnected.");
+                //_tprintf(TEXT("InstanceThread: client disconnected.\n"));
+            }
+            else
+            {
+                auto err = GetLastError();
+                Log(L"InstanceThread ReadFile failed, GLE=" + std::to_wstring(err));
+                //_tprintf(TEXT("InstanceThread ReadFile failed, GLE=%d.\n"), );
+            }
+            break;
+        }
+
+        Log(L"受信data：" + std::wstring(pchRequest));
+
+        // Process the incoming message.
+        GetAnswerToRequest(pchRequest, pchReply, &cbReplyBytes);
+
+        // Write the reply to the pipe. 
+        fSuccess = WriteFile(
+            hPipe,        // handle to pipe 
+            pchReply,     // buffer to write from 
+            cbReplyBytes, // number of bytes to write 
+            &cbWritten,   // number of bytes written 
+            NULL);        // not overlapped I/O 
+
+        if (!fSuccess || cbReplyBytes != cbWritten)
+        {
+            _tprintf(TEXT("InstanceThread WriteFile failed, GLE=%d.\n"), GetLastError());
+            break;
+        }
+    }
+
+    // Flush the pipe to allow the client to read the pipe's contents 
+    // before disconnecting. Then disconnect the pipe, and close the 
+    // handle to this pipe instance. 
+
+    FlushFileBuffers(hPipe);
+    DisconnectNamedPipe(hPipe);
+    CloseHandle(hPipe);
+
+    HeapFree(hHeap, 0, pchRequest);
+    HeapFree(hHeap, 0, pchReply);
+
+    printf("InstanceThread exiting.\n");
+    return 1;
+}
+
+VOID GetAnswerToRequest(LPTSTR pchRequest,
+    LPTSTR pchReply,
+    LPDWORD pchBytes)
+    // This routine is a simple function to print the client request to the console
+    // and populate the reply buffer with a default data string. This is where you
+    // would put the actual client request processing code that runs in the context
+    // of an instance thread. Keep in mind the main thread will continue to wait for
+    // and receive other client connections while the instance thread is working.
+{
+    _tprintf(TEXT("Client Request String:\"%s\"\n"), pchRequest);
+
+    // Check the outgoing message to make sure it's not too long for the buffer.
+    if (FAILED(StringCchCopy(pchReply, BUFSIZE, TEXT("default answer from server"))))
+    {
+        *pchBytes = 0;
+        pchReply[0] = 0;
+        printf("StringCchCopy failed, no outgoing message.\n");
+        return;
+    }
+    *pchBytes = (lstrlen(pchReply) + 1) * sizeof(TCHAR);
+}
+// ダイアログプロシージャ
+BOOL CALLBACK MyDlgProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM lp)
+{
+    ghDlg = hDlg;
+
+    switch (msg) {
+    case WM_COMMAND:
+        switch (LOWORD(wp)) {
+        case IDOK:
+            EndDialog(hDlg, IDOK);
+            return TRUE;
+        case IDCANCEL:
+            EndDialog(hDlg, IDCANCEL);
+            return TRUE;
+        case IDC_BUTTON1:
+        {
+            // pipe クライアント
+            PipeClient();
+            break;
+        }
+        case IDC_BUTTON2:
+            // pipe サーバー
+            PipeServer();
+            break;
+        }
+        return FALSE;
+    }
+    return FALSE;
+}
